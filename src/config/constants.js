5bafa8f80c2f3c0126eb5d3a0f161cd3423f558b
Aditya Patil <125861448+adityapatil2843@users.noreply.github.com>
2026-05-10 12:13:08 +0530

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const WHATSAPP_NUMBER = "919527231973";
 export const INSTAGRAM_URL = "https://instagram.com/anjalipatil26459";
-export const INSTAGRAM_FEED_API = "https://feeds.behold.so/faHILXk6MExfTnTv5b4D"; // Add Behold.so or Instagram API URL here
+export const INSTAGRAM_FEED_API = "https://feeds.behold.so/yZtUwKnBZYDiNsZGqLjA"; // Add Behold.so or Instagram API URL here
